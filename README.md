52293c5a8367fdd2b041f7d309ca96b4ca1c6594
Josh Pollard <49789046+Joshlucpoll@users.noreply.github.com>
2020-11-23 12:44:50 +0000

    Added binary search + cleaned up code

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # name-splitter
-Primarily for NeoRaffle program, takes email in first column of csv andf attempts to extract the first and surname from it.
+Primarily for NeoRaffle program, takes email in first column of csv and attempts to extract the first and surname from it.
--- nameSplitter.py
+++ nameSplitter.py
@@ -1,35 +1,75 @@
-#split names from email
+# split names from email
 from string import digits
 
-with open("names.txt","r") as r:
-    names = r.read().splitlines()
-    names.reverse()
 
-with open("tasks.csv","r") as r:
-    tasks = r.read().splitlines()
+def readFiles():
+    with open("names.txt", "r") as r:
+        names = r.read().splitlines()
 
-finishedTasks = []
+    with open("tasks.csv", "r") as r:
+        tasks = r.read().splitlines()
+
+    return names, tasks
+
+
+def binarySearch(arr, x): 
+    low = 0
+    high = len(arr) - 1
+    mid = 0
+  
+    while low <= high: 
+  
+        mid = (high + low) // 2
+        # shortens prefix so only same number of characters are compared with names list
+        string = x[:len(arr[mid])].capitalize()
+        comparison = arr[mid].capitalize()
+
+        # Check if x is present at mid 
+        if comparison < string: 
+            low = mid + 1
+  
+        # If x is greater, ignore left half 
+        elif comparison > string: 
+            high = mid - 1
+  
+        # If x is smaller, ignore right half 
+        else: 
+            return mid, string
+  
+    # If we reach here, then the element was not present 
+    return False, False;
+
+
+def completeTask(task, names):
+    taskSpilt = task.split(",")
+    
+    email = taskSpilt[0]
+    originalFirstName = taskSpilt[1]
+    originalLastName = taskSpilt[2]
+
+    # removes numbers from prefix
+    remove_digits = str.maketrans('', '', digits)
+    prefix = email.split("@")[0].capitalize().translate(remove_digits)
+
+    found, string = binarySearch(names, prefix)
+
+    if found:
+        newFirstName = string
+        newLastName = prefix.replace(string, "").capitalize()
+
+    if not found:
+        newFirstName = newLastName = prefix
+
+    return str(email+","+newFirstName+","+newLastName+","+taskSpilt[3]+","+taskSpilt[4]+","+taskSpilt[5] +
+                             ","+taskSpilt[6]+","+taskSpilt[7]+","+taskSpilt[8]+","+taskSpilt[9]+","+taskSpilt[10]+","+taskSpilt[11]+"\n")
+
+
+
+names, tasks = readFiles()
+final = ""
 
 for task in tasks:
-    tasksplt = task.split(",")
-    email = tasksplt[0]
-    ogfname = tasksplt[1]
-    oglname = tasksplt[2]
-    remove_digits = str.maketrans('', '', digits) # to remove  numbers from prefix
-    prefix = email.split("@")[0].capitalize().translate(remove_digits)
-    worked = False
-    for name in names:
-        if name in prefix:
-            worked = True
-            fname = name
-            lname = prefix.replace(name,"").capitalize()
-            break
-    if not worked:
-        fname = lname = prefix
-    finishedTasks.append(str(email+","+fname+","+lname+","+tasksplt[3]+","+tasksplt[4]+","+tasksplt[5]+","+tasksplt[6]+","+tasksplt[7]+","+tasksplt[8]+","+tasksplt[9]+","+tasksplt[10]+","+tasksplt[11]+"\n"))
+    final += completeTask(task, names)
 
-final = ""
-for task in finishedTasks:
-    final += task
-with open("form-done.csv","w") as r:
+with open("form-done.csv", "w") as r:
     r.write(final)
